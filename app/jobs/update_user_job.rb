aa50b4eedad2e2ffbf7487da7596f988830f786d
Raquel Ciusz <raquel.ciusz@yahoo.com>
2023-01-10 11:03:46 -0300

    redis bug fixed, dashboard working to admin users

--- app/jobs/update_user_job.rb
+++ app/jobs/update_user_job.rb
@@ -1,7 +1,8 @@
 class UpdateUserJob < ApplicationJob
   queue_as :default
 
-  def perform(user)
+  def perform(user_id)
+    user = User.find(user_id)
     puts "Calling Clearbit API for #{user.email}..."
     # TODO: perform a time consuming task like Clearbit's Enrichment API.
     sleep 2
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_save :call_clearbit
+
+  private
+
+  def call_clearbit
+    UserJob.perform_later(self.id)
+  end
 end
